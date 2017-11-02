096e88293487c305bad8a20d08d043f4d26badb1
r05922028 <r05922028@ntu.edu.tw>
2017-11-02 12:51:27 +0800

    pdf-curve

--- NVM_Analytical_Module/RRAM_distribution.py
+++ NVM_Analytical_Module/RRAM_distribution.py
@@ -2,28 +2,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.mlab as mlab
 import math
+import sys
 
-
-def Normal_distribution(mu, variance):
+def print_cur(a, b, n, func, mu, variance):
 	sigma = math.sqrt(variance)
-	x = np.linspace(mu - 5 * sigma, mu + 5* sigma)
+	x = np.arange(a,b,(b-a)/n)
+	plt.plot(x, [func(each, mu, variance) for each in x])
 	plt.plot(x, mlab.normpdf(x, mu, sigma))
-	plt.show()
+	plt.show()	
 
-def pdf(x):
-	return math.exp(-(x) ** 2 / (2)) / (math.sqrt(2*math.pi))
+#f(x) = (1/sigma*math.sqrt(2pi))* exp(-(x-m)^2/2sigma^2)
+def pdf(x, mu, var):
+	return math.exp(-(x-mu) ** 2 / (2 * math.sqrt(var) ** 2)) / (math.sqrt(2*math.pi) * math.sqrt(var))
 
-def sum_func_xk(xk, func):
-	return sum([func(each) for each in xk])
+def sum_func_xk(xk, func, mu, var):
+	return sum([func(each, mu, var) for each in xk])
 
-def integral(a, b, n, func):
+def integral(a, b, n, func, mu, var):
 	h = (b-a)/float(n) #把lowerbnd 和 upperbnd 之間切成n等分
 	xk = [a + i*h for i in range (1,n)] 
-	return h/2 * (func(a) + 2 * sum_func_xk(xk, func) + func(b))
+	return h/2 * (func(a, mu, var) + 2 * sum_func_xk(xk, func, mu, var) + func(b, mu, var))
 
 #http://www.cnblogs.com/zhangte/p/6156212.html
 def cal_area(lowerbnd, upperbnd, mu, var): 
-	return integral((lowerbnd-mu)/var, (upperbnd-mu)/var, 10000, pdf)
+	return integral(lowerbnd, upperbnd, 10000, pdf, mu, var)
 
-	
-print(cal_area(2,3,2,1))
+
+cell_LRS_mu = sys.argv[1]
+cell_LRS_var = sys.argv[2] 
+cell_HRS_mu = sys.argv[3]
+cell_HRS_var = sys.argv[4] 
+RRAM_size = sys.argv[5]
+
+
+print_cur(-10, 10, 10000, pdf, 2, 10)  
+
+
+
+print(cal_area(-6,10,2,1))
+
