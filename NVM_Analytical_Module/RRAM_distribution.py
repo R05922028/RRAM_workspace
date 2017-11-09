9690a89813cf10543a63abff61a62e15359697b0
r05922028 <r05922028@ntu.edu.tw>
2017-11-09 13:18:14 +0800

    correct distribution

--- NVM_Analytical_Module/RRAM_distribution.py
+++ NVM_Analytical_Module/RRAM_distribution.py
@@ -5,9 +5,9 @@ import math
 import sys
 
 cell_LRS_mu = sys.argv[1]
-cell_LRS_var = sys.argv[2] 
+cell_LRS_sig = sys.argv[2] 
 cell_HRS_mu = sys.argv[3]
-cell_HRS_var = sys.argv[4] 
+cell_HRS_sig = sys.argv[4] 
 RRAM_size = sys.argv[5]
 vol = 0.3 #voltage
 #x_axis = []
@@ -15,49 +15,49 @@ y_axis = []
 LRS = []
 
 
-def print_cur_current(a, b, func, mu, variance):
-	sigma = math.sqrt(variance)
+def print_cur_current(a, b, func, mu, sig):
 	x = np.arange(a,b,(b-a)/10000)
-	plt.plot(0.3/x, [func(each, mu, variance) for each in x])
+	plt.plot(0.3/x, [func(each, mu, sig) for each in x])
 #	x_axis = 0.3/x
-	y_axis = [func(each, mu, variance) for each in x]
+	y_axis = [func(each, mu, sig) for each in x]
 	return y_axis
 
 
-def print_cur(a, b, func, mu, variance):
-	sigma = math.sqrt(variance)
+def print_cur(a, b, func, mu, sigma):
 	x = np.arange(a,b,(b-a)/10000)
-	plt.plot(x, [func(each, mu, variance) for each in x])
+	plt.plot(x, [func(each, mu, sigma) for each in x])
 
 #f(x) = (1/sigma*math.sqrt(2pi))* exp(-(x-m)^2/2sigma^2)
 #http://www2.kuas.edu.tw/prof/tsungo/www/Publish/Normal%20Distribution.pdf
-def pdf(x, mu, var):
-	return math.exp(-(np.log10(x)-mu) ** 2 / (2 * math.sqrt(var) ** 2)) / (math.sqrt(2*math.pi) * math.sqrt(var))
+def pdf(x, mu, sig):
+	return 1/(sig * np.sqrt(2 * np.pi)) * np.exp(-(np.log10(x) - mu)**2 / (2 * sig**2) )
+	#return math.exp(-(np.log10(x)-mu) ** 2 / (2 * (sig ** 2))) / (math.sqrt(2*math.pi) * sig)
 
 
-def sum_func_xk(xk, func, mu, var):
-	return sum([func(each, mu, var) for each in xk])
+def sum_func_xk(xk, func, mu, sig):
+	return sum([func(each, mu, sig) for each in xk])
 
-def integral(a, b, func, mu, var):
+def integral(a, b, func, mu, sig):
 	h = (b-a)/float(10000)
 	xk = [a + i*h for i in range (1,10000)] 
-	return h/2 * (func(a, mu, var) + 2 * sum_func_xk(xk, func, mu, var) + func(b, mu, var))
+	return h/2 * (func(a, mu, sig) + 2 * sum_func_xk(xk, func, mu, sig) + func(b, mu, sig))
 
 #http://www.cnblogs.com/zhangte/p/6156212.html
-def cal_area(lowerbnd, upperbnd, mu, var): 
-	return integral(lowerbnd, upperbnd, pdf, mu, var)
+def cal_area(lowerbnd, upperbnd, mu, sig): 
+	return integral(lowerbnd, upperbnd, pdf, mu, sig)
 
 
 distr = []
-#print_cur(1, 25000, pdf, float(cell_LRS_mu) , float(cell_LRS_var))  
-distr.append(print_cur_current(1, 25000, pdf, float(cell_LRS_mu) , float(cell_LRS_var)))
-distr.append(print_cur_current(1, 25000, pdf, float(cell_HRS_mu) , float(cell_HRS_var)))
+#print_cur(1, 100, pdf, float(cell_LRS_mu) , float(cell_LRS_sig))  
+#print_cur(1, 10000, pdf, float(cell_HRS_mu) , float(cell_HRS_sig))  
+distr.append(print_cur_current(1, 10000, pdf, float(cell_LRS_mu) , float(cell_LRS_sig)))
+distr.append(print_cur_current(1, 10000, pdf, float(cell_HRS_mu) , float(cell_HRS_sig)))
 print(len(distr))
 print(len(distr[0]))
-
+print(cal_area(1,100, 1.34, 0.06))
 
 #plt.xscale('log')
-plt.axis([-0.01, 0.15, 0, 2])
+plt.axis([0,0.025, 0, 8])
 plt.show()	
 
 
