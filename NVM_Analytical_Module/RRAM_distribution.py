6b3bc40c157eb95fafa51228f27da2caa41d0a62
R05922028 <r05922028@ntu.edu.tw>
2017-11-14 02:20:00 +0800

    current_distribution_&_integral

--- NVM_Analytical_Module/RRAM_distribution.py
+++ NVM_Analytical_Module/RRAM_distribution.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+from scipy import integrate
+from scipy.integrate import simps
 import numpy as np
 import matplotlib.mlab as mlab
 import math
@@ -14,68 +16,47 @@ max_prob = 0
 new_mu = 0
 
 
-
-def print_cur_current(a, b, func, mu, sig):
-	x = np.arange(a,b,(b-a)/10000)
-	plt.plot(0.3/x, [func(each, mu, sig) for each in x])
-
-
 def print_cur(a, b, func, mu, sigma):
-  x = np.arange(a,b,(b-a)/10000)
-  plt.plot(x, [func(each, mu, sigma) for each in x])
+	x = np.arange(a,b,(b-a)/10000)
+	plt.plot(x, [func(each, mu, sigma) for each in x])
 def cal_new_mu(a, b, func, mu, sigma):
-  x = np.arange(a,b,(b-a)/10000)
-  [func(each, mu, sigma) for each in x]
-  global max_prob
-  max_prob = 0
-  global new_mu
-  return new_mu
+	x = np.arange(a,b,(b-a)/10000)
+	[func(each, mu, sigma) for each in x]
+	global max_prob
+	max_prob = 0
+	global new_mu
+	return new_mu
 
 #f(x) = (1/sigma*math.sqrt(2pi))* exp(-(log(x)-m)^2/2sigma^2)
 def pdf_log(x, mu, sig):
 	return 1/x/(sig * np.sqrt(2 * np.pi)) * np.exp(-(np.log(x) - mu)**2 / (2 * sig**2) )
 
 def pdf_current(x, mu, sig):
-  return 1/x/(sig * np.sqrt(2 * np.pi)) * np.exp(-(np.log(vol/x) - mu)**2 / (2 * sig**2) )
+	return (1/(x)/(sig * np.sqrt(2 * np.pi))) * np.exp(-(np.log(vol/x) - mu)**2 / (2 * sig**2) )
 
-def pdf_max(x, mu, sig):
-  #return 1/(sig * np.sqrt(2 * np.pi)) * np.exp(-(np.log10(vol/x) - mu)**2 / (2 * sig**2) )
-  prob = 1/x/(sig * np.sqrt(2 * np.pi)) * np.exp(-(np.log(vol)-np.log(x)- mu)**2 / (2 * sig**2) )
-  global max_prob
-  if(prob > max_prob):
-    max_prob = prob
-    global new_mu
-    new_mu = np.log(x)
-  return prob
-def sum_func_xk(xk, func, mu, sig):
-	return sum([func(each, mu, sig) for each in xk])
 
 def integral(a, b, func, mu, sig):
-	h = (b-a)/float(10000)
-	xk = [a + i*h for i in range (1,10000)] 
-	return h/2 * (func(a, mu, sig) + 2 * sum_func_xk(xk, func, mu, sig) + func(b, mu, sig))
+	h = (b-a)/float(1000000)
+	xk = [a + i*h for i in range (1,1000000)] 
+	xk = np.array(xk)
+	pdf = func(xk, mu, sig)
+	return integrate.simps(pdf, xk)
 
-#http://www.cnblogs.com/zhangte/p/6156212.html
-def cal_area(lowerbnd, upperbnd, mu, sig): 
-	return integral(lowerbnd, upperbnd, pdf_log, mu, sig)
 
-cell_LRS_current_mu = cal_new_mu(0.00001, 0.1, pdf_max, float(cell_LRS_mu), float(cell_LRS_sig)) 
-cell_HRS_current_mu = cal_new_mu(0.00001, 0.1, pdf_max, float(cell_HRS_mu), float(cell_HRS_sig)) 
-print(cell_LRS_current_mu)
-print(cell_HRS_current_mu)
-  
 
-print_cur(1,10000, pdf_log, float(cell_LRS_mu), float(cell_LRS_sig))
+#print_cur(1,50, pdf_log, float(cell_LRS_mu), float(cell_LRS_sig))
+print_cur(0.000001,0.1, pdf_current, float(cell_LRS_mu), float(cell_LRS_sig))
+print_cur(0.000001,0.1, pdf_current, float(cell_HRS_mu), float(cell_HRS_sig))
 #print_cur(0.001,1, pdf_log, float(cell_LRS_current_mu), float(cell_LRS_sig))
 
 #print_cur(1,1000, pdf_log, float(cell_LRS_mu), float(cell_LRS_sig))
 #print_cur(0.00001,0.1, pdf_current, float(cell_LRS_mu), float(cell_LRS_sig))
 #print_cur(0.00001,0.1, pdf_current, float(cell_HRS_mu), float(cell_HRS_sig))
-#print_cur_current(1, 10000, pdf_log, float(cell_LRS_mu), float(cell_LRS_sig))  
-print(cal_area(1,1000, cell_LRS_mu, cell_LRS_sig))
+print(cell_LRS_mu)
+print(integral(0.00001,1, pdf_current, float(cell_LRS_mu), float(cell_LRS_sig)))
 
 plt.xscale('log')
-#plt.axis([0,100, 0, 0.2])
+#plt.axis([0.01,100, 0, 0.2])
 plt.show()	
 
 
