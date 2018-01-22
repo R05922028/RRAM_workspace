9f948b9194f4d72c4ec0e18c1ed3eb5ad86810f4
r05922028 <r05922028@ntu.edu.tw>
2018-01-22 20:09:52 +0800

    modify

--- Error_simulation/restore.py
+++ Error_simulation/restore.py
@@ -3,34 +3,30 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 # Testing data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-
 # Restore Model
 sess = tf.InteractiveSession()
 
-saver = tf.train.import_meta_graph('./model/cnn_model.ckpt.meta')
-saver.restore(sess, './model/cnn_model.ckpt')
 #saver.restore(sess, tf.train.latest_checkpoint( './model/'))
 
 
 graph = tf.get_default_graph()
 #print(sess.run(graph.get_tensor_by_name('b:0')))
 #print(sess.run('w:0'))
+saver = tf.train.import_meta_graph('model/cnn_model.ckpt.meta')
+saver.restore(sess, tf.train.latest_checkpoint('model/'))
 
 x = graph.get_tensor_by_name('x:0')
+
 y_ = graph.get_tensor_by_name('y_:0')
 prediction = graph.get_tensor_by_name('prediction:0')
 
 
 # Testing
-#print(mnist.test.labels)
-pre = sess.run(prediction, feed_dict={x:mnist.test.images})
-#print(pre)
-correct_prediction = tf.equal(tf.argmax(pre,1), tf.argmax(y_,1))
+correct_prediction = tf.equal(tf.argmax(prediction,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print("accuracy: ",sess.run(accuracy, feed_dict={y_:mnist.test.labels}))
+print(accuracy.eval({x: mnist.test.images, y_: mnist.test.labels}))
 
 # Visualize
 writer = tf.summary.FileWriter('TensorBoard/', graph=sess.graph)
 
 sess.close()
-
